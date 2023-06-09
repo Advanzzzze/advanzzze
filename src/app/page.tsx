15484e13fd460d1b882b404d7cf69c4b079c0fb4
Lymei <lymeicontato@gmail.com>
2023-06-09 17:52:46 -0300

    fix: metadata do not working in layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import BlurSpot from '@/components/BlurSpot';
 import HomeHero from '@/components/Home/HomeHero';
 
+export const metadata = {
+	title: 'Advanzzze',
+	description:
+		'Desenvolvedores especializados em sites, aplicativos e designs, impulsionando seu sucesso digital. Junte-se a nós e avance para o próximo nível.',
+};
+
 export default function Home() {
 	return (
 		<main className='flex justify-center overflow-y-hidden'>
